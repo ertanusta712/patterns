ec92f81d4ac3b46ba808b9e6f0e2b9b53e4f4a81
Ertan USTA <ertan.usta@ideasoft.com.tr>
2021-07-19 00:53:29 +0300

    .

--- adapter/index.php
+++ adapter/index.php
@@ -1,4 +1,8 @@
 <?php
+require('Database.php');
+require('MongoDB.php');
+require('MongoDBConnection.php');
+require('MysqlConnection.php');
 $mysql = new MysqlConnection("test","test","test","test");
 
 $mongo = new MongoDBConnection(new MongoDB("test","test","test","test"));
--- bridge/index.php
+++ bridge/index.php
@@ -1,5 +1,11 @@
 <?php
-
+require('BaseModel.php');
+require('BaseRepository.php');
+require('Connection.php');
+require('MongoConnection.php');
+require('ProductModel.php');
+require('MysqlConnection.php');
+require('ProductRepository.php');
 //mysql connection ı ile mysql veritabanına kayıt eder
 $productRepository = new ProductRepository(new MysqlConnection("test","test","test","test"));
 $productRepository->save(new ProductModel());
